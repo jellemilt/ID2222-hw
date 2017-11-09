42542edaa06f266bc67d75dd868fea7544919e4a
Jellevanmiltenburg <jellemilt@hotmail.com>
2017-11-09 18:17:27 +0100

    Unfinished jaccard similarity

--- src/main/scala/SimilarItems.scala
+++ src/main/scala/SimilarItems.scala
@@ -1,7 +1,7 @@
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
-import scala.collection.SortedSet
+import scala.collection.{SortedSet, mutable}
 import scala.util.Random
 
 object SimilarItems {
@@ -40,8 +40,44 @@ object SimilarItems {
 
   // A class that calculates the Jaccard Similarity out of two Sets of Integers
   def calculateJaccardSimilarity(doc1: Set[Int], doc2: Set[Int]): Double = {
-    var placeholer: Double = 0.5
-    return placeholer
+    var intersect: Int = 0
+    var union: Int = 0
+
+    def compare_values(val1: Set[Int], val2: Set[Int], p1: Int, p2: Int): Double = {
+      if (val1.isEmpty && val2.isEmpty )
+        return union.toDouble/intersect.toDouble
+      else if (val1.isEmpty || val2.isEmpty && val1(p1) > val2(p2))
+        intersect += 1
+      compare_values(val1, val2, p1, p2 +1)
+      else if (val1.isEmpty || val2.isEmpty && val1(p1) < val2(p2))
+        intersect += 1
+      compare_values(val1, val2, p1 + 1, p2)
+      else
+      intersect += 1
+      union += 1
+      compare_values(val1, val2, p1 + 1, p2 +1)
+    }
+
+    val pos_doc1: Int = 0
+    val pos_doc2: Int = 0
+
+    compare_values(doc1, doc2, pos_doc1, pos_doc2)
+
+  }
+
+  def compareSets(A: Set[Int], B: Set[Int]): Double = {
+    def compare(A: SortedSet[Int], B: SortedSet[Int], sizeIntersection: Int, sizeUnion: Int): Double = {
+      if (A.isEmpty && B.isEmpty)
+        sizeUnion.toDouble / sizeIntersection.toDouble
+      else if (A.isEmpty || B.nonEmpty && A.head > B.head)
+        compare(A, B.tail, sizeIntersection + 1, sizeUnion)
+      else if (B.isEmpty || A.nonEmpty && A.head < B.head)
+        compare(A.tail, B, sizeIntersection + 1, sizeUnion)
+      else // A.head == B.head
+        compare(A.tail, B.tail, sizeIntersection + 1, sizeUnion + 1)
+    }
+
+    compare(A.to[SortedSet], B.to[SortedSet], 0, 0)
   }
 
 
