3f03b46274c9d21159e44c43eb5ced3f53038f03
Jellevanmiltenburg <jellemilt@hotmail.com>
2017-11-11 18:26:07 +0100

    Lab1 Finished

--- src/main/scala/SimilarItems.scala
+++ src/main/scala/SimilarItems.scala
@@ -1,7 +1,8 @@
 import org.apache.spark.{SparkConf, SparkContext}
-
+import org.apache.spark.rdd.RDD
 import scala.collection.SortedSet
 import scala.util.Random
+
 
 object SimilarItems {
   def main(args: Array[String]) {
@@ -10,78 +11,142 @@ object SimilarItems {
       .setAppName("Similar Items")
       .setMaster("local[*]")
     val sc = new SparkContext(conf)
-    val filePath = "src/Data/Part1/awards_1990/awd_1990_01/"
+    val filePath = "src/Data/Part1/awards_1990/awd_1990_00/"
 
     val docs = sc.wholeTextFiles(filePath).cache()
       .mapValues(text => text.replace("\\s+", " ")) //replace line breaks with space
-      .map({case (path, text) => (path.split("/").last, text)}) //store only name of the document, instead of the
-    // whole path
-
-    //docs.take(5).foreach(println)
+      .map({case (path, text) => (path.split("/").last, text)}) //store only name of the document
 
     //make shingles and hash the shingles to a Int value
-    val hashedShingles = docs.mapValues(makeShingles(_, 3))
-      .mapValues(_.map(_.hashCode.intValue()).to[SortedSet]) // Hash the shingles to
-      // hashCode and sort the list
-      //.collectAsMap()
+    val hashedShingles = docs.mapValues(makeShingles(_, 5))
+      .mapValues(_.map(_.hashCode.intValue()).to[SortedSet]) // Hash the shingles to hashCode and sort the list
 
-    val jaccard_val = hashedShingles.cartesian(hashedShingles).map({case (x,y) => jacccardsimple(x._2,y._2)})
-    jaccard_val.take(5).foreach(println) //(a9001001.txt,TreeSet(-2145272195, -2142988454, -2140567169, ..))
-
-
-
-    //*************************************************************
-    //THIS IS WHERE MY PART STARTS
-    //YOU CAN CHANGE THE REST TO WHATEVER THE FUCK
-    //IF THE CODE DOWN HERE DOESNT WORK THEN; JUST COMMENT IT
-    //ILL FIX IT THEN AFTERWARDS
-    //*************************************************************
+    //Use minHasher to turn hashedShingles into a signature
     val minHasher = new MinHashing(1000000000,100)
-    val minHashedShingles = hashedShingles.mapValues{hashedShingle => minHasher.minHash(hashedShingle.toSet)}.collectAsMap()
+    val minHashedShingles = hashedShingles.mapValues{hashedShingle => minHasher.minHash(hashedShingle.toSet)}
+    minHashedShingles.take(5).foreach(println)
+    //val keys = minHashedShingles.collectAsMap().keySet
 
-    val keys = minHashedShingles.keySet
-    hashedShingles.take(2).foreach(println)
-    //minHashedShingles.take(2).foreach(println)
-    //println((minHashedShingles apply keys.head)) //(a9001001.txt,TreeSet(-2145272195, -2142988454, -2140567169, ..))
+    //Use Locality Sensitive Hashing to dertermine candidate pairs of document, based on their signature
+    val candidateDocuments = LSH(minHashedShingles)
+
+
+    //***** Compare documents *****
+
     val compareSignatures = new CompareSignatures
-    println((minHashedShingles apply keys.head).size)
-    println(keys.getClass)
-    minHashedShingles.foreach{
-        case(key1,mhs1)=>
-          minHashedShingles.foreach{
-            case(key2,mhs2)=>{
-              val similarity = compareSignatures.compareVectors(mhs1,mhs2)
-              if (similarity >0.6f && similarity < 1)
-                println(key1+" and "+key2+" -> "+similarity)
-            }}}
+    val t = 0.85 // threshold of similarity
+
+    //compare candidate documents by their signatures
+    val comparedSignatures = candidateDocuments.map({case (doc1, doc2) => (doc1, doc2, signatureSimilarity(doc1._2, doc2._2))})
+
+    //keep only the pairs whose similarity is above the threshold
+    val similarDocumentsWRTSignature = comparedSignatures
+      .filter({case (doc1, doc2, score) => score >= t})
+
+    //Compare documents based on their shingles
+    val comparedShingles = similarDocumentsWRTSignature.map({case (doc1,doc2,score) =>
+      val jaccardScore = jaccardSimilarity(doc1._2,doc2._2)
+      (doc1,doc2,jaccardScore)})
+
+    //Keep only the paris whose similarity is above threshold
+    val similarDocumentsWRTShingles = comparedShingles
+      .filter({case (doc1, doc2, score)=> score >= t})
+
+//    val prettifiedSimilarItems = similarDocumentsWRTShingles
+//      .map({case (doc1, doc2, score) => (doc1._1, doc2._1, score)})
 
 
-  }
+    //***** Print results *****
 
-  // A function that creates a set of shingles out of an string input
+    println("Resulting LSH candidate pairs, filtered by their signature comparison")
+    similarDocumentsWRTSignature.collect().foreach({case(doc1,doc2,score)=> println("%s and %s | %.4f".format(doc1._1, doc2._1, score))})
+
+    println("LSH candidate pairs, filtered by signature comparison, and by the shingle comparison")
+    similarDocumentsWRTShingles.collect().foreach({case(doc1,doc2,score)=> println("%s and %s | %.4f".format(doc1._1, doc2._1, score))})
+
+
+
+//
+//    println((minHashedShingles.collectAsMap() apply keys.head).size)
+//    println(keys.getClass)
+//    minHashedShingles.foreach{
+//        case(key1,mhs1)=>
+//          minHashedShingles.foreach{
+//            case(key2,mhs2)=>{
+//              val similarity = compareSignatures.compareVectors(mhs1,mhs2)
+//              if (similarity >0.6f && similarity < 1)
+//                println(key1+" and "+key2+" -> "+similarity)
+//            }}}
+
+
+    }
+
+  //******* Functions *******
+
+
+  // 1.) A function that creates a set of shingles out of an string input
   def makeShingles(document: String, k: Int): Set[String] = {
     (0 until k)
       .flatMap(document.substring(_).grouped(k))
-      .filter(_.length == k).distinct
+      .filter(_.length == k)
       .toSet
   }
-
-  def jacccardsimple(doc1: SortedSet[Int], doc2: SortedSet[Int]): Double = {
-    doc1.intersect(doc2).size.toDouble/(doc1 ++ doc2).size.toDouble
+  // 2.) A function that calculates the similarity of two documents based on their hashed shingles
+  def jaccardSimilarity(doc1: List[Int], doc2: List[Int]): Double = {
+    val (sorteddoc1, sorteddoc2) = (doc1.to[SortedSet], doc2.to[SortedSet])
+    val score = sorteddoc1.intersect(sorteddoc2).size.toDouble/(sorteddoc1 ++ sorteddoc2).size.toDouble
+    score
   }
-  // A class that calculates the Jaccard Similarity out of two Sets of Integers
 
+  // 3.) A Function that creates a signature out of a list of hashed shingles
   def minHashing(shingle: Set[Int], hashAmount: Int): Set[Int] = {
     //defines how many possibilities there are for hash functions
     val randomness = 10000
     //start by defining -hashAmount- different hashs of the form y=mx+t
     val hashs = List.fill(hashAmount)(Random.nextInt(randomness),Random.nextInt(randomness))
     val minHashs = hashs.map{case(m,t) => shingle.map(m*_+t).min}
-    //No modulo needed. It would have to be Modulo 2^32 which is the size of Int -> Int modulos it by default
-    return minHashs.toSet
+    //No modulo needed. It would have to be Modulo 2^32 which isulos  the size of Int -> Int modit by default
+    minHashs.toSet
+  }
+  // 4.) A function that calculates the similarity of two documents based on their signatures
+  def signatureSimilarity(sign1: List[Int], sign2: List[Int]): Double = {
+    val joinedVecs = sign1 zip sign2
+    var sameCount=0f
+    joinedVecs.foreach{case(v1,v2) => if(v1==v2){sameCount += 1f}}
+    sameCount/joinedVecs.size
+    //sign1.zip(sign2).count({ case (a, b) => a == b }).toDouble / sign1.size.toDouble
   }
 
+  // 5.) A function that calculates candidate pairs that might be similar, based on bands of their signatures
+  def LSH(docs: RDD[(String, List[Int])]): RDD[((String, List[Int]), (String, List[Int]))] = {
+
+    val numberOfBuckets = 1000000 //number of buckets
+    val bandSize = 5
+
+    //make buckets and hash all bands of all documents in the different buckets
+    val candidatePairs = docs.flatMap({ case (name, sign) =>
+        sign.grouped(bandSize).map(piece => {
+          val bucket = piece.hashCode%numberOfBuckets
+          (bucket, (name, sign))})})
+
+    //Get all pairs of document of which at least one band falls in the same bucket
+
+      .groupByKey() //group by bucket
+      .flatMap({case (bucket, listOfDocs) => listOfDocs.cross(listOfDocs)}) // create a cartesian product of all docs in
+      //a bucket
+      .map({case (doc1, doc2) => if (doc1._1 < doc2._1)(doc1, doc2) else (doc2, doc1)}) //make sure that all pairs are
+      //similarly ordered
+      .filter({case (doc1, doc2) => doc1 != doc2}) //keep only the pairs that are not identical
+      .distinct()//remove duplicates
 
 
+    candidatePairs
+
+  }
+
+// do cross product (src: https://stackoverflow.com/questions/14740199/cross-product-in-scala)
+  implicit class Crossable[X](xs: Traversable[X]) {
+    def cross[Y](ys: Traversable[Y]) = for { x <- xs; y <- ys } yield (x, y)
+  }
 
 }
